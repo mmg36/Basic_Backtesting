ffed6b518387bc94bc5869b2ef4e98ad50b1d444
Mehdi <mmg36@cam.ac.uk>
2014-08-24 01:05:20 +0100

    Wrong version Just use for Cleaning data files

--- Scratch.py
+++ Scratch.py
@@ -1,15 +1,15 @@
 __author__ = 'mehdi'
-
+import numpy as np
 
 def rand1():
 
     def1 = 0
-
+    arr1 = [1, 2, 3, 4]
     for count in xrange(1, 10):
         def1 += 1
+        arr1.append( def1)
 
-    print def1
-
-print "Hello"
+    print arr1
+    print "Hello"
 
 rand1()
